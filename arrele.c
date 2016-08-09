2c140a07022471cac89bdfce9c6c95d6581901ea
SabarishDhanarajan <sabarishdhanarajan@gmail.com>
2016-08-09 21:09:23 +0530

    Create arrele.c

---
+++ arrele.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int a[100],b[100];
+    int n;
+    int i,j,k=0,x=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            for(k=0;k<n;k++)
+            {
+                if((a[i*2]+a[j*2])-a[k*2]==0)
+                {
+                    x=1;
+                    break;
+                }
+            }
+            if(x==1)
+            break;
+        }
+        if(x==1)
+        break;
+    }
+    printf("%d\t%d\t%d",i,j,k);
+    return 0;
+}
